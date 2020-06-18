b93cabd6bd0dd55146733c65bffe1e010eac5452
JonnyJaccob <57542964+JonnyJaccob@users.noreply.github.com>
2020-06-18 03:00:31 -0500

    Add files via upload

--- Applicacion2Ejer/Clock.cs
+++ Applicacion2Ejer/Clock.cs
@@ -12,27 +12,38 @@ namespace Applicacion2Ejer
     {
         public bool Comprobar = false;
         public event PropertyChangedEventHandler PropertyChanged;
+        //public int Cuentan=0;
+        public int Cuentame=0;
+        string Solo;
         public void RaisedPropertyChanged(String propiedad)
         {
             if (PropertyChanged != null)
             {
+                
                 PropertyChanged(this, new PropertyChangedEventArgs(propiedad));
+                PropertyChanged(this, new PropertyChangedEventArgs(Solo));
+                Cuentame++;
             }
         }
         
-        private int _Hora;
+        public int _Hora;
         public Clock(int hora)
-        {
+        {   
+            
             this.Hora = hora;
             //INICIAMOS EL TIMER, FUNCIONANDO CADA SEGUNDO
             Device.StartTimer(TimeSpan.FromSeconds(1), () =>
             {
                 this.Hora--;
+                Cuentan = _Hora;
+                Cuentame++;
+                Solo = $"{Cuentame}";
+                Aumentar();
                 //EN EL MOMENTO QUE LLEGUE A CERO LO PARAMOS
                 if (Hora == 0)
                 {
                     //GENERAMOS LA NOTIFICACION                        
-                    //CrossLocalNotifications.Current.Show("Notificaciones", "El lapso de tiempo finalizo finalizado. Tiempo transcurrido: " + hora);
+                    CrossLocalNotifications.Current.Show("Ejercicio Terminado!!!!", "BUEN TRABAJO!!!. Tiempo transcurrido: " + hora);
                     Comprobar = true;
                     return false;
                 }
@@ -40,11 +51,23 @@ namespace Applicacion2Ejer
                     Comprobar = false;
                     return true;
             });
+            
+        }
+        void Aumentar()
+        {
+            Cuentame++;
         }
         public int Hora
         {
             get { return this._Hora; }
             set { this._Hora = value; RaisedPropertyChanged("Hora"); }
         }
+        private int _Cuentan = 0;
+        public int Cuentan
+        {
+            get { return this._Cuentan; }
+            set { this._Cuentan = value; RaisedPropertyChanged("Cuentan"); }
+        }
+    
     }
 }
